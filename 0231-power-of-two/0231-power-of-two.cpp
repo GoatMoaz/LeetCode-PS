7d8abe00ddd1c9ccc8974d319b28c105db07780b
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-02-28 15:00:50 +0200

    Time: 5 ms (19.45%), Space: 7.3 MB (32.32%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)
+            return false;
+        else if ((n&(n-1))==0)
+            return true;
+        else
+            return false;
+    }
+};
