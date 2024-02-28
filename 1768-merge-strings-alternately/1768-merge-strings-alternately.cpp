491dd33ace499ba1e4c126e67265815c216c01a7
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-02-28 15:09:54 +0200

    Time: 0 ms (100.00%), Space: 7.7 MB (15.00%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        bool w1=false,w2=false;
+        for(int i=0 ; i<min(word1.size(),word2.size()) ; i++){
+            ans.push_back(word1[i]);
+            ans.push_back(word2[i]);
+        }
+        if(word1.size()>word2.size()){
+            for(int i=word2.size() ; i<word1.size() ; i++){
+                ans.push_back(word1[i]);
+            }
+        }
+        else if (word2.size()> word1.size()){
+            for(int i=word1.size() ; i<word2.size() ; i++){
+                ans.push_back(word2[i]);
+            }
+        }
+        return ans;
+    }
+};
