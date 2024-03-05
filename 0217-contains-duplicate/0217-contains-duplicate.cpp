c8e317b322dfd9b54284b26b7b46f804e44b9593
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-05 17:15:42 +0200

    Time: 85 ms (83.74%), Space: 60.9 MB (79.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,10 +3,11 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s;
-        for(int i=0 ; i<nums.size() ; i++){
-            s.insert(nums[i]);
+        sort(nums.begin() , nums.end());
+        for(int i=1 ; i<nums.size() ; i++){
+            if(nums[i]==nums[i-1])
+                return true;
         }
-        return s.size()<nums.size();
+        return false;
     }
 };
