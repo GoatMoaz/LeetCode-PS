0d261aadad014837849cd9263f2efae319a069b2
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-07 19:55:28 +0200

    Time: 0 ms (100.00%), Space: 7.1 MB (64.75%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,14 +3,20 @@ using namespace std;
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        bitset<32> bitA(a),bitB(b),bitC(c);
         int ans = 0;
-        for (int i = 0; i < bitA.size(); ++i) {
-            if ((bitA[i] | bitB[i]) != bitC[i]) {
-                if (((!bitA[i] | bitB[i]) == bitC[i]) || ((bitA[i] | !bitB[i]) == bitC[i]))
+        for (int i = 0; i < 32; ++i) {
+            bool ai = (a& (1<<i));
+            bool bi = (b& (1<<i));
+            bool ci = (c& (1<<i));
+            if(!ci){
+                if(ai && bi)
+                    ans+=2;
+                else if (ai || bi)
                     ans++;
-                else
-                    ans += 2;
+            }
+            else{
+                if(!ai && !bi)
+                    ans++;
             }
         }
         return ans;
