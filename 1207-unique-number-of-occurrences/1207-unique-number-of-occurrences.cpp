d9cf2e6202cc2527e562d6316c2a7c3eb4a32e95
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-07 19:04:55 +0200

    Time: 0 ms (100.00%), Space: 11.4 MB (6.20%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        unordered_set<int> s;
+        for (int i = 0; i < arr.size(); ++i) {
+            mp[arr[i]]++;
+        }
+        for (auto i: mp) {
+            s.insert(i.second);
+        }
+        return s.size()==mp.size();
+    }
+};
