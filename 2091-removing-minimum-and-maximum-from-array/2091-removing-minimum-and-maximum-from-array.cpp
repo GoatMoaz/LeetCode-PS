af4fa581214c0173c3e897d6019aa836bfc465e1
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-09 15:12:09 +0200

    Time: 123 ms (45.63%), Space: 90.8 MB (15.01%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -5,28 +5,17 @@ public:
     int minimumDeletions(vector<int>& nums) {
         if (nums.size() == 1)
             return 1;
-        int min_moves_left = 1e5, max_moves_left = -1e5, min_moves_right = 1e5, max_moves_right = -1e5;
+        int min_moves = 1e5, max_moves = -1e5;
         int max_value = -1e5, min_value = 1e5;
         for (int i = 0; i < nums.size(); ++i) {
-            max_value = max(max_value, nums[i]);
-            min_value = min(min_value, nums[i]);
+            if (max_value < nums[i])
+                max_value = nums[i], max_moves = i;
+            if (min_value > nums[i])
+                min_value = nums[i], min_moves = i;
         }
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == min_value)
-                min_moves_left = i + 1;
-            else if (nums[i] == max_value)
-                max_moves_left = i + 1;
-        }
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            if (nums[i] == min_value)
-                min_moves_right = nums.size() - i;
-            else if (nums[i] == max_value)
-                max_moves_right = nums.size() - i;
-        }
-        return min(
-                {max(min_moves_right,max_moves_right),
-                 max_moves_left + min_moves_right,
-                 max_moves_right + min_moves_left,
-                 max(min_moves_left,max_moves_left)});
+        int left = max(max_moves + 1, min_moves + 1);
+        int right = max(nums.size() - max_moves, nums.size() - min_moves);
+        int mid = min(max_moves + 1, min_moves + 1) + min(nums.size() - max_moves, nums.size() - min_moves);
+        return min({left, right, mid});
     }
 };
