8cf247642f808cd186bf181bcdaed4dd5fb47cb0
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-11 16:38:04 +0200

    Time: 3 ms, Memory: 12.8 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,12 +1,14 @@
+#define GOAT  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,int> mp;
+        GOAT
+        int mp[26]{0};
         for (int i = 0; i < s.size(); ++i) {
-            mp[s[i]]++;
+            mp[s[i]-'a']++;
         }
         for (int i = 0; i < s.size(); ++i) {
-            if(mp[s[i]]==1)
+            if(mp[s[i]-'a']==1)
                 return i;
         }
         return -1;
