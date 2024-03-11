d83a39ce9ecb428cc683967f6ce02c9462160eca
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-11 15:56:58 +0200

    Time: 54 ms, Memory: 40.5 MB - LeetHub

--- minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -1,27 +1,19 @@
-bool sortbysec(const pair<string,int> &a , const pair<string,int> &b){
-    if(a.second==b.second)
-        return (a.first>b.first);
-    return (a.second < b.second);
-}
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        vector<pair<string,int>> v;
-        for (int i = 0; i < list1.size(); ++i) {
-            for (int j = 0; j < list2.size(); ++j) {
-                if(list1[i]==list2[j]) {
-                    v.push_back({list1[i],i+j});
+        unordered_map<string, int> mp;
+        vector<string> ans;
+        int mn = 1e5;
+        for (int i = 0; i < list1.size(); ++i)
+            mp[list1[i]] = i;
+        for (int i = 0; i < list2.size(); ++i) {
+            if (mp.count(list2[i]) > 0) {
+                if (i + mp[list2[i]] <= mn) {
+                    if (i + mp[list2[i]] < mn)
+                        ans.clear(), mn = i + mp[list2[i]];
+                    ans.push_back(list2[i]);
                 }
             }
-        }
-        sort(v.begin(),v.end(), sortbysec);
-        vector<string> ans;
-        ans.push_back(v[0].first);
-        for (int i = 1; i < v.size(); ++i) {
-            if(v[i].second==v[i-1].second)
-                ans.push_back(v[i].first);
-            else
-                break;
         }
         return ans;
     }
