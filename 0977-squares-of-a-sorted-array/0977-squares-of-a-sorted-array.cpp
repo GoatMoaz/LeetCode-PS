72e1a2050233932518c26974ad7ef15ce05aacf0
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-03-28 21:31:38 +0200

    Time: 18 ms (84.44%), Space: 28.3 MB (85.26%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
+#define GOAT  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0 ; i<nums.size() ; i++){
-            nums[i]*=nums[i];
+        GOAT
+        int l=0,r=nums.size()-1,indx=r;
+        vector<int> ans(nums.size());
+        while(l<=r){
+            if(abs(nums[l])>nums[r]) {
+                ans[indx--] = nums[l] * nums[l];
+                l++;
+            }
+            else{
+                ans[indx--] = nums[r] * nums[r];
+                r--;
+            }
         }
-        sort(nums.begin() , nums.end());
-        return nums;
+        return ans;
     }
 };
