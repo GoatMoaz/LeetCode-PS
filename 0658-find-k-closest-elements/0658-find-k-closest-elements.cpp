b13013a2d4ea7160a0a18acf76d362477d2458fc
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-04-14 18:14:54 +0200

    Time: 20 ms (94.65%), Space: 33.6 MB (87.43%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,23 +1,16 @@
 #define GOAT  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-bool sortbysec(const pair<int,int> &a , const pair<int,int> &b){
-    if(a.second==b.second)
-        return (a.first<b.first);
-    return (a.second < b.second);
-}
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         GOAT
-        vector<int> ans;
-        vector<pair<int,int>> v;
-        for (int i = 0; i < arr.size(); ++i) {
-            v.push_back({arr[i],abs(arr[i]-x)});
+        int l=0,r=arr.size()-1;
+        while(r-l>=k){
+            if(abs(arr[l]-x)>abs(arr[r]-x))
+                l++;
+            else
+                r--;
         }
-        sort(v.begin() , v.end() , sortbysec);
-        for (int i = 0; i < k; ++i) {
-            ans.push_back(v[i].first);
-        }
-        sort(ans.begin() , ans.end());
+        vector<int> ans(arr.begin()+l , arr.begin()+r+1);
         return ans;
     }
 };
