aca5c2888fe6e69be209233d3a9c3098a7cb1ac6
Moaz Ayman <120432015+GoatMoaz@users.noreply.github.com>
2024-04-16 14:37:56 +0200

    Time: 7 ms (19.79%), Space: 12.5 MB (12.82%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,18 +4,13 @@ public:
     vector<int> finalPrices(vector<int>& prices) {
         GOAT
         stack<int> st{{0}};
-        vector<int> ans(prices.size());
         for (int i = 1; i < prices.size(); ++i){
             while(!st.empty() && prices[st.top()]>=prices[i]) {
-                ans[st.top()] = prices[st.top()] - prices[i];
+                prices[st.top()] -= prices[i];
                 st.pop();
             }
             st.push(i);
         }
-        while (!st.empty()){
-            ans[st.top()]=prices[st.top()];
-            st.pop();
-        }
-        return ans;
+        return prices;
     }
 };
